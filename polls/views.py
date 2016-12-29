a3c8449052d5e5cfa0438780136eb38230c9f96c
Chris <cnp.a.002@gmail.com>
2016-12-29 22:33:15 +0200

    Created model and views

--- polls/views.py
+++ polls/views.py
@@ -7,7 +7,7 @@ from .models import Question, Choice
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by('pub_date')[:5];
+    latest_question_list = Question.objects.order_by('pub_date')[:5]
     output = " | ".join([q.question_text for q in latest_question_list])
     return HttpResponse(output)
 
